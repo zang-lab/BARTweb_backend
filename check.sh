31977bf21cb370973e9685b72ad0b21dbe5c7de8
sygy1995 <yz4hc@virginia.edu>
2019-05-29 13:18:36 -0400

    debugging

--- check.sh
+++ check.sh
@@ -36,6 +36,8 @@ if [ "$QCOUNT" -gt 0 ]; then
   echo $RAW >> $LOG_dir/aws_queue.log 2>&1;
   # The $DIR variable is the name of your submissionkey
   DIR=`echo $RAW | /usr/bin/jq -r .Messages[0].MessageAttributes.submissionkey.StringValue`;
+  attr=`echo $RAW | /usr/bin/jq -r .Messages[0].MessageAttributes`;
+  echo $attr >> $LOG_dir/aws_queue.log 2>&1;
   echo $DIR >> $LOG_dir/aws_queue.log 2>&1;
   # execute the trigger.py
   /usr/bin/python3 $BART_dir/trigger.py $DIR >> $LOG_dir/aws_queue.log 2>&1;
